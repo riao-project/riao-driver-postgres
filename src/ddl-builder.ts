6dcb11f92c40bb045c2daa46917fdad7574a85fa
Drew Immerman <drewimmerman@gmail.com>
2023-04-08 17:15:25 -0400

    Add support for auto-increment (serial)

--- src/ddl-builder.ts
+++ src/ddl-builder.ts
@@ -1,3 +1,29 @@
-import { DataDefinitionBuilder } from 'riao-dbal/src';
+import {
+	ColumnOptions,
+	ColumnType,
+	DataDefinitionBuilder,
+} from 'riao-dbal/src';
 
-export class PostgresDataDefinitionBuilder extends DataDefinitionBuilder {}
+export class PostgresDataDefinitionBuilder extends DataDefinitionBuilder {
+	public getAutoIncrement(): string {
+		return '';
+	}
+
+	public createTableColumn(column: ColumnOptions): string {
+		if ('autoIncrement' in column && column.autoIncrement) {
+			column.autoIncrement = false;
+
+			if (column.type === ColumnType.SMALLINT) {
+				column.type = <any>ColumnType.SMALLSERIAL;
+			}
+			else if (column.type === ColumnType.INT) {
+				column.type = <any>ColumnType.SERIAL;
+			}
+			else if (column.type === ColumnType.BIGINT) {
+				column.type = <any>ColumnType.BIGSERIAL;
+			}
+		}
+
+		return super.createTableColumn(column);
+	}
+}
