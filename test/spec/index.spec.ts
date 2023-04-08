4da4656f7d7a9a6b24c101d24382c75514b0aaa3
Drew Immerman <drewimmerman@gmail.com>
2023-04-08 09:21:36 -0400

    Project created using TypeScript Template (lib)

--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -1,7 +1,7 @@
 import 'jasmine';
 import * as index from '../../src';
 
-describe('{{project-name}}', () => {
+describe('riao-driver-postgres', () => {
 	it('exports a', () => {
 		expect(index.a).toBeTrue();
 	});
