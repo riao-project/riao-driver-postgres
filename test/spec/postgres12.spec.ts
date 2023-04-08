a3ae0d3b5b46283d1ac65657a2534fde56197d23
Drew Immerman <drewimmerman@gmail.com>
2023-04-08 16:08:53 -0400

    Update driver-test setup

--- test/spec/postgres12.spec.ts
+++ test/spec/postgres12.spec.ts
@@ -1,11 +1,11 @@
 import 'jasmine';
-import { Postgres12Driver } from '../../src';
+import { DatabasePostgres12 } from '../../src';
 import { connectionOptionsPostgres12 } from '../connection-options';
 import { test } from 'riao-driver-test/src';
 
 test({
 	name: 'Postgres 12',
-	driver: Postgres12Driver,
+	db: DatabasePostgres12,
 	expectedVersion: /^12\.[0-9]+$/,
 	connectionOptions: connectionOptionsPostgres12,
 });
--- test/spec/postgres14.spec.ts
+++ test/spec/postgres14.spec.ts
@@ -1,11 +1,11 @@
 import 'jasmine';
-import { Postgres14Driver } from '../../src';
+import { DatabasePostgres14 } from '../../src';
 import { connectionOptionsPostgres14 } from '../connection-options';
 import { test } from 'riao-driver-test/src';
 
 test({
 	name: 'Postgres 14',
-	driver: Postgres14Driver,
+	db: DatabasePostgres14,
 	expectedVersion: /^14\.[0-9]+$/,
 	connectionOptions: connectionOptionsPostgres14,
 });
