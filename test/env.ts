969b434f60db860382bd6b85911245507ef44d3e
Drew Immerman <drewimmerman@gmail.com>
2023-11-24 11:18:19 -0500

    Fixed performance issue with docker postgres
    https://github.com/docker-library/postgres/issues/1147

--- test/env.ts
+++ test/env.ts
@@ -6,28 +6,28 @@ import { AppConfig, configure } from 'ts-appconfig';
 export class Environment extends AppConfig {
 	readonly APP_TITLE = 'typescript-template-library';
 
-	readonly TEST_POSTGRES12_HOST = 'localhost';
+	readonly TEST_POSTGRES12_HOST = '0.0.0.0';
 	readonly TEST_POSTGRES12_PORT = 5432;
 	readonly TEST_POSTGRES12_USERNAME = 'riao_root';
 	readonly TEST_POSTGRES12_PASSWORD = 'password1234';
 	readonly TEST_POSTGRES12_DATABASE = 'riaodb';
 	readonly TEST_POSTGRES12_ROOT_DATABASE = 'db';
 
-	readonly TEST_POSTGRES13_HOST = 'localhost';
+	readonly TEST_POSTGRES13_HOST = '0.0.0.0';
 	readonly TEST_POSTGRES13_PORT = 5433;
 	readonly TEST_POSTGRES13_USERNAME = 'riao_root';
 	readonly TEST_POSTGRES13_PASSWORD = 'password1234';
 	readonly TEST_POSTGRES13_DATABASE = 'riaodb';
 	readonly TEST_POSTGRES13_ROOT_DATABASE = 'db';
 
-	readonly TEST_POSTGRES14_HOST = 'localhost';
+	readonly TEST_POSTGRES14_HOST = '0.0.0.0';
 	readonly TEST_POSTGRES14_PORT = 5434;
 	readonly TEST_POSTGRES14_USERNAME = 'riao_root';
 	readonly TEST_POSTGRES14_PASSWORD = 'password1234';
 	readonly TEST_POSTGRES14_DATABASE = 'riaodb';
 	readonly TEST_POSTGRES14_ROOT_DATABASE = 'db';
 
-	readonly TEST_POSTGRES15_HOST = 'localhost';
+	readonly TEST_POSTGRES15_HOST = '0.0.0.0';
 	readonly TEST_POSTGRES15_PORT = 5435;
 	readonly TEST_POSTGRES15_USERNAME = 'riao_root';
 	readonly TEST_POSTGRES15_PASSWORD = 'password1234';
