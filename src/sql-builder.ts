a0387420611839c8d3b4ef962a46dd2911792ff3
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2024-01-15 11:54:45 -0500

    #7 like() is case-sensitive, which breaks compatibility with other databases (#8)

--- src/sql-builder.ts
+++ src/sql-builder.ts
@@ -3,6 +3,11 @@ import { SqlBuilder } from '@riao/dbal/builder';
 export class PostgresSqlBuilder extends SqlBuilder {
 	protected placeHolderId = 1;
 
+	public operators = {
+		...this.operators,
+		like: 'ILIKE',
+	};
+
 	public appendPlaceholder(): this {
 		// postgres uses incrementing placeholders in the format $1, $2,...
 		this.sql += `\$${this.placeHolderId} `;
