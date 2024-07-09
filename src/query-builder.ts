98436ef516517054d9673fed16ebe59c0961cf88
Drew Immerman <drewimmerman@gmail.com>
2024-07-09 18:30:01 -0400

    DBAL-51 Add support for year()

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -1,5 +1,6 @@
-import { DatabaseQueryBuilder } from '@riao/dbal';
+import { DatabaseFunctions, DatabaseQueryBuilder } from '@riao/dbal';
 import { PostgresSqlBuilder } from './sql-builder';
+import { DatabaseFunction } from '@riao/dbal/functions/function-token';
 
 export class PostgresQueryBuilder extends DatabaseQueryBuilder {
 	public getSqlType() {
@@ -14,6 +15,21 @@ export class PostgresQueryBuilder extends DatabaseQueryBuilder {
 
 	public uuid(): this {
 		this.sql.append('gen_random_uuid()');
+
+		return this;
+	}
+
+	public override year(fn: DatabaseFunction) {
+		this.sql.append('DATE_PART(\'year\', ');
+
+		if (fn.params?.expr) {
+			this.expression(fn.params.expr);
+		}
+		else {
+			this.expression(DatabaseFunctions.currentTimestamp());
+		}
+
+		this.sql.closeParens();
 
 		return this;
 	}
