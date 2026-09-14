a447faf832720f8657b5bd19ba415f542a7155d6
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2026-09-14 13:17:14 -0400

    feat: add support for month() (#20)
    riao-dbal#52

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -54,6 +54,21 @@ export class PostgresQueryBuilder extends DatabaseQueryBuilder {
 		return this;
 	}
 
+	public override month(fn: DatabaseFunction) {
+		this.sql.append('DATE_PART(\'month\', ');
+
+		if (fn.params?.expr) {
+			this.expression(fn.params.expr);
+		}
+		else {
+			this.expression(DatabaseFunctions.currentTimestamp());
+		}
+
+		this.sql.closeParens();
+
+		return this;
+	}
+
 	public override setStatement(): this {
 		return this;
 	}
